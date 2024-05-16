51793fd3332c84dbfca005af674c6e804798c648
George Papoutsas <osfpgeopap1925@gmail.com>
2024-05-16 04:05:38 +0300

    chore: Refactor main.py to use FrankaRobot class for tasks 1 and 2

--- main.py
+++ main.py
@@ -2,63 +2,50 @@ import numpy as np
 import FrankaRobot as fr
 import pinocchio as pin
   
-def task1(robot): 
-
-    model = robot.model
-    T = 10
-    dt = 0.01
+def task1(robot:fr.Franka, T:int, dt:float, q0:np.ndarray, u:np.ndarray, control_t:np.ndarray, visuals:bool=False): 
+    """
+    Simulate the robot for a certain time period with a given control torque and visualize the robot
     
-    
-    ## Initial state
-    # Position
-    # pin.seed(1083738)
-    q0 = pin.randomConfiguration(model)
-    # q0 = np.array([0.0, 0.0, 0.0, -1.57, 0.0, 1.57, 0.0])
-    # q0 = np.array([ 1.13380891, -0.56585662, -2.65877219, -2.05434817, -2.81552492,  0.18592432, 0.28310915])
+    Args:
+    - robot: Franka robot object
+    - T: time period
+    - dt: time step
+    - q0: initial position
+    - u: initial velocity
+    - control_t: control torque
+    - visuals: whether to visualize the robot or not
+    """
 
-    # Velocity
-    u = np.zeros(model.nv)
-
-    # Control torque
-    control_t = np.array([0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-
+    # Simulate the robot
     qs, end_state = robot.simulate(q0, u, control_t, T, dt)
     q, u = end_state
     print(f"\t\t\tEnd state\nposistion: {end_state[0]}\nvelocity: {end_state[1]}")
     
     # Visualize the robot
-    # visualize(robot, qs)
-    # while True: continue
+    if visuals:
+        robot.visualize(robot, qs)
+        while True: continue
 
-def task2(robot):
-
-    model = robot.model
-
-    T = 10
-    dt = 0.01
+def task2(robot:fr.Franka, T:int, dt:float, q0:np.ndarray, u:np.ndarray, control_t:np.ndarray, target_q:np.ndarray, Kp:float=100., Ki:float=1., Kd:float=0., visuals:bool=False):
+    """
+    Simulate the robot for a certain time period with a given control torque 
+    and use a Task Space Controller to reach a certain target pose profile 
+    and visualize the robot
     
-    ## Initial state
-    # Position
-    # pin.seed(1083738)
-    # q0 = pin.randomConfiguration(model)
-    q0 = np.array([0.0, 0.0, 0.0, -1.57, 0.0, 1.57, 0.0])
-    # q0 = np.array([ 1.13380891, -0.56585662, -2.65877219, -2.05434817, -2.81552492,  0.18592432, 0.28310915])
+    Args:
+    - robot: Franka robot object
+    - T: time period
+    - dt: time step
+    - q0: initial position
+    - u: initial velocity
+    - control_t: control torque
+    - target_q: target position
+    - Kp: proportional gain
+    - Ki: integral gain
+    - Kd: derivative gain
+    - visuals: whether to visualize the robot or not"""
 
-    # Velocity
-    u = np.zeros(model.nv)
-
-    ## Control torque
-    control_t = np.array([0.0, 5.0, 0.0, 0.0, 4.0, 0.0, 0.0])
-    
-    # Target configuration
-    target_q = np.array([ 1.13380891, -0.56585662, -2.65877219, -2.05434817, -2.81552492,  0.18592432, 0.28310915])
-
-    Kp = 100.
-    Ki = 1.
-    Kd = 0.
-
-    qs, end_state = robot.simulate(q0, u , control_t, T, dt, target_q, Kp, Ki, Kd)
-    # qs, end_state = simulate(model, data, q0, u , control_t, T, dt, target_q, Kp, Ki, Kd)
+    qs, end_state = robot.simulate(q0, u, control_t, T, dt, target_q, Kp, Ki, Kd)
     q, u = end_state
 
     print(f"\nEnd posistion:\t\tTarget position:")
@@ -66,19 +53,43 @@ def task2(robot):
         print(f"{q[i]:.3f}\t\t\t{target_q[i]:.3f}")
     
     # Visualize the robot
-    robot.visualize(qs)
-    while True: continue
+    if visuals:
+        robot.visualize(robot, qs)
+        while True: continue
 
-def task3(robot):    
+def task3(robot, T, dt, q0, u, control_t):    
     pass
 
 
 if __name__ == "__main__":
 
     robot = fr.Franka()
+    model = robot.model
 
-    task1(robot)
+    # Time period
+    T = 10
 
-    # task2(robot)
+    # Time step
+    dt = 0.1
+    
+    ## Initial state
+    # Joints configuration
+    q0 = pin.randomConfiguration(model)
 
-    # task3(robot)
+    # Velocity
+    u = np.zeros(model.nv)
+
+    ## Control torque
+    control_t = np.array([0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+    # task1(robot, T, dt, q0, u, control_t)
+
+    Kp = 100.
+    Ki = 1.
+    Kd = 0.
+
+    target_q = np.array([ 1.13380891, -0.56585662, -2.65877219, -2.05434817, -2.81552492,  0.18592432, 0.28310915])
+
+    task2(robot, T, dt, q0, u, control_t, target_q, Kp, Ki, Kd)
+
+    # task3(robot, T, dt, q0, u, control_t)
